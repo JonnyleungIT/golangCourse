a45b5bd69b0ebaf4f86fb4a538daec70bb51b712
jonnyleung <13413750839@163.com>
2019-06-18 23:50:45 +0800

    更新

--- code/helloworld.go
+++ code/helloworld.go
@@ -1,9 +1,12 @@
+// 导入主函数的包
 package main
 
-import {
-	"fmt"
-}
+// 导入程序所依赖的包
+import "fmt" // 标准输入输出包
 
-func main(){
-	fmt.Pringln("helloworld!")
+// main主函数，程序主入口，一个go文件有且只有一个主函数
+
+func main()  {
+	// 打印helloworld
+	fmt.Println("helloworld")
 }
